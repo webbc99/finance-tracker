bde2706ae0aff6ba3e1c617341a47d9675b0d417
Rails Udemy Course <webbc99@gmail.com>
2018-05-21 15:37:14 +0100

    Added user and stock associations, plus views

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 	root 'users#my_portfolio'
 	get 'my_portfolio', to: 'users#my_portfolio'
 	get 'search_stocks', to: 'stocks#search'
+	resources :user_stocks, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
