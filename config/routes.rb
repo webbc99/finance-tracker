66453ebdbf5780862dd4d1bbcf6ae222c3dd38a0
Rails Udemy Course <webbc99@gmail.com>
2018-05-21 21:59:00 +0100

    Added table and model for friendships

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 	root 'welcome#index'
 	get 'my_portfolio', to: 'users#my_portfolio'
 	get 'search_stocks', to: 'stocks#search'
+	get 'my_friends', to: 'users#my_friends'
 	resources :user_stocks, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
