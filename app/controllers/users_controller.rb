7533e89afb678868551ecf2b25674de86a51ffa3
Rails Udemy Course <webbc99@gmail.com>
2018-05-22 18:06:46 +0100

    Added the adding and removing of friends functionality. App is now ready for production use

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,37 @@ class UsersController < ApplicationController
 	end
 
 	def my_friends
-		
+		@friendships = current_user.friends
+	end
+
+	def search
+		if params[:search_param].blank?
+			flash.now[:danger] = "Please enter a string to search."
+		else
+			@users = User.search(params[:search_param])
+			@users = current_user.except_current_user(@users)
+			flash.now[:danger] = "Your search returned no results." if @users.blank?
+		end
+		respond_to do |format|
+			format.js { render partial: 'friends/result'}
+		end
+	end
+
+	def add_friend
+		@friend = User.find(params[:friend])
+		current_user.friendships.build(friend_id: @friend.id)
+
+		if current_user.save
+			flash[:success] = "Friend was successfully added."
+		else
+			flash[:danger] = "There was something wrong with the friend request, please try again."
+		end
+		redirect_to my_friends_path
+	end
+
+	def show
+		@user = User.find(params[:id])
+		@user_stocks = @user.stocks
 	end
 
 end
